e311be834c7dd815c649d366840366692def3c02
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-09-25 14:38:23 +0900

    商品出品機能

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,5 +8,9 @@ FactoryBot.define do
     prefecture_id {0}
     day_ship_id {2}
     price {300}
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
