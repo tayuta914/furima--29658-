423ef0dfaf03fa992807d4ef14243c6dbe00b789
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-09-25 18:49:54 +0900

    商品出品機能

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,6 +5,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :Shipping_charge
   belongs_to_active_hash :Prefecture
   belongs_to_active_hash :Day_ship
+  belongs_to :user
   has_one_attached :image
 
   validates :image, :name, :text, :price, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :day_ship_id, presence: true
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -8,7 +8,11 @@ FactoryBot.define do
     prefecture_id {0}
     day_ship_id {2}
     price {300}
+
     association :user
-  
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
