aa600bb7882f2567473c4516bf7d32b30124e88f
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-09-26 20:43:32 +0900

    商品一覧機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :create]
   def index
-    @items = Item.all
+    @items = Item.includes(:item).order("created_at DESC")
   end
 
   def new
