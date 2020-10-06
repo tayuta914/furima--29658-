e96f674f35c13206b6bd8284a812453630eff433
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-10-06 17:39:41 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,4 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  # 環境変数を読み込む記述に変更
     end
   end
-
-end
-
 end
