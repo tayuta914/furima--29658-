fb552a7b55e419a918ffc5814412fade49cb066b
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-10-06 14:33:24 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,11 +36,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-     @item.destroy
+    if @item.user_id == current_user.id && @item.destroy
      return redirect_to root_path
     else
-     render :edit
+     render :show
     end
   end
 
